44641f9a235bd69fde2cbc557ff65fc051143222
Paul S <landofalcon007@gmail.com>
2020-03-06 13:32:31 -0600

    Created Project 0

---
+++ Project 0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Project_0
+{
+    class Program
+    {
+        
+    // place orders to store locations for customers
+    // add a new customer
+    // search customers by name
+    // display details of an order
+    // display all order history of a store location
+    // display all order history of a customer
+    // input validation
+    // (optional: order history can be sorted by earliest, latest, cheapest, most expensive)
+    // (optional: get a suggested order for a customer based on his order history)
+    // (optional: display some statistics based on order history)
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
