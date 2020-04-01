72945ffba89caf8aadbbd92b6dec7daacb969308
Paul S <landofalcon007@gmail.com>
2020-04-01 10:14:36 -0500

    #1 Removed unneccessary Order param from Login method

--- DataAccess/Entities/Stores.cs
+++ DataAccess/Entities/Stores.cs
@@ -17,7 +17,6 @@ namespace DataAccess.Entities
         public int? Reviews { get; set; }
         public string PhoneNumber { get; set; }
 
-        public virtual Inventory Inventory { get; set; }
         public virtual ICollection<OrderHistory> OrderHistory { get; set; }
         public virtual ICollection<Reviews> ReviewsNavigation { get; set; }
     }
